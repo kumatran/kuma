a5006b3b574ef30ba975987d13f6284e320437d2
kumatran <kumatran.ohara@gmail.com>
2020-12-26 20:13:10 +0700

    re

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("adventure time Distand land");
+        System.out.println("adventure time");
 
 
 
