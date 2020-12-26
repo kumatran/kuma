8703cbd1c1cdf060b56a3bdd70d0d422efe52679
kumatran <kumatran.ohara@gmail.com>
2020-12-26 20:17:57 +0700

    ADVDT

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Jake the dog");
+        System.out.println("Jake the dog and Fin the human");
 
 
 
