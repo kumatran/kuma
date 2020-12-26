478d40cbaf3f269b72de45f7638f075950cd12e5
VTCA_STUDENTS <kumatran.ohara@gmail.com>
2020-12-26 19:25:13 +0700

    TEST COMMIT

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("adventure time JD-FH");
+        System.out.println("adventure time");
 
 
 
